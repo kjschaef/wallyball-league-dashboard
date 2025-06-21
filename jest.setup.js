5bccb75f11e8c79f4bea0e89907f2077f855d71e
Keith Schaefer <keith.schaefer@cfacorp.com>
2025-06-21 00:13:36 +0000

    cleanup

--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-require('@testing-library/jest-dom');
-
-global.fetch = jest.fn();
-
-if (typeof TextEncoder === 'undefined') {
-  global.TextEncoder = require('util').TextEncoder;
-  global.TextDecoder = require('util').TextDecoder;
-}
